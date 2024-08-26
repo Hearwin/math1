d4ea7f0a42f7c688cdeb67f3310eef439c9223d0
janvichr001 <janvichr001@warren.k12.in.us>
2024-08-26 10:15:18 -0400

    VARIABLES MAIN COMMIT

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 frisbees = 3 #pretend we got from user input
- Cost_of_fribees = 15
- total_cost = 15 * 3
- print ( "45$", 45 , sep ="" )
+Cost_of_fribees = 15
+total_cost = frisbees * Cost_of_fribees
+print("The total cost is $", total_cost, sep="")
